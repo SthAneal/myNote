7a0a926f1a76e8b563b15bbf262c0f8bb8180432
sthAneal <anilmeronaam@gmail.com>
2022-05-29 17:14:08 +1000

    new register user

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+type userInfo = {
+    name:string
+  }
 
 export const Register = ()=>{
+    const [user, setUser] = useState<userInfo>({} as userInfo);
+
+    useEffect(()=>{
+        getName();
+    }, [])
+
+    const getName = async ()=>{
+        const response = await axios.get('/http://localhost:3000/api/home');
+        console.log(response);
+        setUser({name:response.data});
+      }
     return(
-        <div>Register</div>
+        <div>Register:{user.name}</div>
     )
 }
