be4ff05bb47637b81abb1f629f341cf3823baf5d
sthAneal <anilmeronaam@gmail.com>
2022-05-29 17:24:11 +1000

    login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
 import React, {useState, useEffect} from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import axios from 'axios';
+// import axios from 'axios';
 import './App.css';
 import {Login} from './components/Login';
 import {Register} from './components/Register';
 import {Home} from './components/Home';
 
 
-type userInfo = {
-  name:string
-}
+
 
 function App() {
-  const [user, setUser] = useState<userInfo>({} as userInfo);
 
-  useEffect(()=>{
-    getName();
-  }, [])
 
   // const getName = async ()=>{
   //   // const response = await axios.get('/name');
@@ -26,16 +20,12 @@ function App() {
 
   // }
 
-  const getName = async ()=>{
-    const response = await axios.get('/http://localhost:3000/api');
-    console.log(response);
-    setUser({name:response.data});
-  }
+
 
   return (
     <>
       <div>User name:</div>
-      <div>{user.name}</div>
+      {/* <div>{user.name}</div> */}
       <Router>
         <Routes>
           <Route path="/login" caseSensitive={false} element={<Login />} />
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+type userInfo = {
+    name:string
+  }
 
 export const Login = ()=>{
+    const [user, setUser] = useState<userInfo>({} as userInfo);
+
+    useEffect(()=>{
+    getName();
+    }, [])
+
+
+    const getName = async ()=>{
+        const response = await axios.get('/http://localhost:3000/api/dashboard');
+        console.log(response);
+        setUser({name:response.data});
+    }
+
     return(
-        <div>Login</div>
+        <div>Login: {user.name}</div>
     )
 }
