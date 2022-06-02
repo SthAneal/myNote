f9ae531781a3481929a2479dc46003682b2846ec
sthAneal <anilmeronaam@gmail.com>
2022-06-02 18:27:52 +1000

    axios production baseurl corrected

--- src/api/Axios.tsx
+++ src/api/Axios.tsx
@@ -1,8 +1,8 @@
 import Axios from 'axios';
 
 export default Axios.create({
-    // baseURL:"http://143.198.234.42",
-    baseURL:"http://localhost:3000",
+    baseURL:"http://143.198.234.42", // for production
+    // baseURL:"http://localhost:3000", // only for local
     withCredentials: false,
     headers:{
         'Access-Control-Allow-Origin':'*',
